c328265a7c3fa2432b24a76f150ef3c60660f14b
kpapakipos <keegan@papakipos.com>
2019-10-29 14:14:13 -0700

    create xcode project

---
+++ Mouse Tracker/Mouse Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mouse Tracker
+//
+//  Created by Keegan Papakipos on 10/29/19.
+//  Copyright © 2019 Keegan Papakipos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
