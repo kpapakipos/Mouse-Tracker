015a24410633731b1539cac19eb2831768d0b021
kpapakipos <keegan@papakipos.com>
2019-11-15 15:45:14 -0800

    added new ViewController subclasses for new adventure screen and more options screen

---
+++ Mouse-Tracker/Mouse-Tracker/MoreOptionsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoreOptionsViewController.swift
+//  Mouse-Tracker
+//
+//  Created by Keegan Papakipos on 11/15/19.
+//  Copyright © 2019 Keegan Papakipos. All rights reserved.
+//
+
+import UIKit
+
+class MoreOptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
